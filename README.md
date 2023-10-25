456295eccb0a1cec3621ebdf0fdaf3326947557e
James Cocker <purplespider@users.noreply.github.com>
2023-10-25 16:17:29 +0100

    Initial Purple Spider Revamp Commit

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-# Silverstripe Social Meta Tags
-Adds social media meta tags to Silverstripe sites.
+# Basic Social Sharing
+Adds social media sharing fields to Silverstripe sites.
 
 ## Description
-**silverstripe-social-meta-tags** adds social media meta tags for OpenGraph (Facebook) and Twitter to SiteTree and child page types.
+This module adds social media meta tags for OpenGraph (Facebook) and Twitter to SiteTree and child page types. It also provides Title, Description and Image fields in the CMS to allow the default values to be overridden.
+
+The module will look for Title, Description and Image values in the following order:
+1. As entered in the fields of the provided "Social Media Sharing" section in the CMS.
+2. The value of a specific field for a specific class, as configured in the YML (see below).
+3. The value of the appropriate field from the CMS "Metadata" section.
+4. The value from the "default" field, as specified in the module config. e.g. Page "Title" and "Content".
+
+The module uses placeholder (and field description) text to indicate what values are being used, allowing you to then override them, only if necessary.
+
+![](screenshots/screenshot-1-defaults.png)
+![](screenshots/screenshot-2-completed.png)
 
 Supported social media meta tags include:
 
@@ -22,12 +33,12 @@ Supported social media meta tags include:
 ## Requirements
 SilverStripe 4.2+
 
-Use the `1.0.1` tag ([ss3 branch](https://github.com/innis-maggiore/silverstripe-social-meta-tags/tree/1.0.1)) for SilverStripe 3 sites.
 
 ## Installation
-1. ``composer require innis-maggiore/silverstripe-social-meta-tags ^4.0``
+1. ``composer require purplespider/silverstripe-basic-social-sharing ^1.0``
 2. run a `/dev/build?flush=all`
-3. Without any configuration, the module will begin rendering social media meta tags for all page types. 
+3. Without any configuration, the module will begin rendering social media meta tags for all page types.
+4. Customise these on a per page/DO basis via the new "Social Media Sharing" section, below the existing "Metadata" section for pages. 
 
 ## Defaults
 By default, the module will use the following fields to populate the meta tags:
@@ -35,10 +46,10 @@ By default, the module will use the following fields to populate the meta tags:
 * `og:type`				--> `"website"`
 * `og:site_name`		--> `SiteConfig.Title`
 * `og:title`			--> `Title`
-* `og:description`		--> `MetaDescription`
+* `og:description`		--> `Content`
 * `og:url`				--> `AbsoluteLink()`
 * `twitter:title`		--> `Title`
-* `twitter:description`	--> `MetaDescription`
+* `twitter:description`	--> `Content`
 * `twitter:card`		--> `"summary"`
 
 NOTE: `twitter:card` will be set to `"summary"` by default, unless an image field is used. If an image field is used, the content will be set to `"summary_large_image"`.
@@ -46,10 +57,9 @@ NOTE: `twitter:card` will be set to `"summary"` by default, unless an image fiel
 ## Configuration
 Configure the module by editing ``mysite/_config/config.yml`` and set the following options:
 ```yml
-InnisMaggiore\SocialMetaTags:
+PurpleSpider\BasicSocialSharing:
   default_title: 'Title' 
-  default_description: 'MetaDescription' 
-  meta_description_default: true 
+  default_description: 'Content' 
   twitter_site: '' 
   titles:	
     Fully\Namespaced\ClassName: 'FieldName'
@@ -66,7 +76,6 @@ InnisMaggiore\SocialMetaTags:
 ```
 * `default_title` - override the default title field to use; `PageTitle` is used by default.
 * `default_description` - override the default description field to use; `MetaDescription` is used by default.
-* `meta_description_default` - flag that indicates whether to fall back to `MetaDescription` if the chosen description field is empty.
 * `twitter_site` - the content to be used for thw `twitter:site` meta tag.
 * `titles` - an array of class names, with the value being the name of the Text field to use for title meta tags. Useful for adding social media meta tag output to DataObjects.
 * `descriptions` - an array of class names, with the value being the name of the Text field to use for description meta tags. Useful for adding social media meta tag output to DataObjects.
@@ -75,25 +84,24 @@ InnisMaggiore\SocialMetaTags:
 
 ## Example `config.yml`
 ```yml
-InnisMaggiore\SocialMetaTags:
+PurpleSpider\BasicSocialSharing:
   default_title: 'PageTitle'
   default_description: ''
-  meta_description_default: true
   titles:
-    InnisMaggiore\Models\Category: 'Name'
-    InnisMaggiore\Models\Product: 'Name'
-    InnisMaggiore\Models\Recipe: 'Name'
+    PurpleSpider\Models\Category: 'Name'
+    PurpleSpider\Models\Product: 'Name'
+    PurpleSpider\Models\Recipe: 'Name'
   descriptions:
-    InnisMaggiore\Models\Category: 'ShortDescr'
-    InnisMaggiore\Models\Product: 'Summary'
-    InnisMaggiore\Models\Recipe: 'MetaDescription'
+    PurpleSpider\Models\Category: 'ShortDescr'
+    PurpleSpider\Models\Product: 'Summary'
+    PurpleSpider\Models\Recipe: 'MetaDescription'
   images:
-    InnisMaggiore\Pagetypes\AboutUsPage: 'LogoImage'
-    InnisMaggiore\Models\Category: 'BeautyShot'
-    InnisMaggiore\Models\Product: 'BeautyShot'
-    InnisMaggiore\Models\Recipe: 'Image'
+    PurpleSpider\Pagetypes\AboutUsPage: 'LogoImage'
+    PurpleSpider\Models\Category: 'BeautyShot'
+    PurpleSpider\Models\Product: 'BeautyShot'
+    PurpleSpider\Models\Recipe: 'Image'
   types:
-    InnisMaggiore\Models\Recipe: 'article'
+    PurpleSpider\Models\Recipe: 'article'
   twitter_site: ''
 ```
 ## Adding social media meta tags to DataObjects
@@ -104,7 +112,7 @@ To add social media meta tags to DataObjects:
 ```yml
 Fully\Namespaced\Dataobject:
   extensions:
-    - InnisMaggiore\SilverstripeSocialMetaTags\SocialMetaTagsExtension
+    - PurpleSpider\BasicSocialSharing\BasicSocialSharingExtension
 ```
 * in `_config/config.yml`, set options for `titles`, `descriptions`, and `images` (if desired) for your class.
   * for an example, see the above example `config.yml`.
@@ -125,3 +133,6 @@ Fully\Namespaced\Dataobject:
 
 ## License
 This project is licensed under the [New BSD License](./LICENSE)
+
+## Original Module
+This is a heavily modified and more opinionated version of https://github.com/innis-maggiore/silverstripe-social-meta-tags. Huge thanks to Innis for the development of the key functionality.
--- src/SocialMetaTagsExtension.php
+++ src/SocialMetaTagsExtension.php
@@ -1,21 +1,25 @@
 <?php
 
-namespace InnisMaggiore\SilverstripeSocialMetaTags;
+namespace PurpleSpider\BasicSocialSharing;
 
-use Silverstripe\ORM\DataExtension;
-use SilverStripe\Forms\FieldList;
-use SilverStripe\Forms\TextareaField;
-use SilverStripe\AssetAdmin\Forms\UploadField;
-use SilverStripe\Core\Convert;
-use SilverStripe\Core\Config\Config;
-use SilverStripe\SiteConfig\SiteConfig;
-use SilverStripe\ErrorPage\ErrorPage;
 use SilverStripe\Assets\Image;
+use SilverStripe\Core\Convert;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use Silverstripe\ORM\DataExtension;
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Forms\LiteralField;
+use SilverStripe\ErrorPage\ErrorPage;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\SiteConfig\SiteConfig;
 use SilverStripe\CMS\Model\RedirectorPage;
+use SilverStripe\Forms\ToggleCompositeField;
+use SilverStripe\AssetAdmin\Forms\UploadField;
 
-class SocialMetaTagsExtension extends DataExtension {
+class BasicSocialSharingExtension extends DataExtension {
     private static $db = array(
+        "SocialMetaTitle" => "Text",
         "SocialMetaDescription" => "Text"
     );
 
@@ -31,9 +35,125 @@ class SocialMetaTagsExtension extends DataExtension {
         $owner = $this->getOwner();
 
         if (is_subclass_of($owner, SiteTree::class) && !is_a($owner, RedirectorPage::class) && !is_a($owner, ErrorPage::class)) {
-            $fields->addFieldToTab('Root.Main.Metadata', TextareaField::create("SocialMetaDescription")->addExtraClass("stacked"));
-            $fields->addFieldToTab('Root.Main.Metadata', UploadField::create("SocialMetaImage")->addExtraClass("stacked"));
+            $fields->addFieldToTab('Root.Main',
+                ToggleCompositeField::create(
+                    'SocialMedia',
+                    'Social Media Sharing',
+                    [
+                        LiteralField::create('SocialInfo', '<div class="field" style="margin-bottom:1.23rem"><p class="alert" style="padding-left:0">Customise the details displayed in preview cards when this page is shared on Facebook, Twitter, LinkedIn etc:</p></div>'),
+                        TextField::create("SocialMetaTitle",'Title')->setAttribute('placeholder', $owner->findSocialTitleText($forPlaceholder = true)),
+                        TextareaField::create("SocialMetaDescription",'Description')->setRows(3)->setAttribute('placeholder', $owner->findSocialMetaDescription($forPlaceholder = true)),
+                        UploadField::create("SocialMetaImage",'Image'),
+                        LiteralField::create('Debug', '<div class="field" style="margin-bottom:1.23rem"><p class="alert alert-warning">If you change these <i>after</i> sharing the page on Facebook, you may need to instruct Facebook to "re-scrape" this page before it will display the updated details. You can do this using the <strong>Scrape Again</strong> button on the <a href="https://developers.facebook.com/tools/debug/?q=' . $this->owner->AbsoluteLink() . '" target="_blank"><strong>Facebook Sharing Debugger</strong></a>.</div>'),
+                        LiteralField::create('ShareLink', '<div class="field" style="margin-bottom:1.23rem"><p style="font-weight:bold; font-size:16px; padding-bottom:3px;display:block;">Share to: <a target="_blank"  href="http://www.facebook.com/share.php?u='.$owner->AbsoluteLink().'">Facebook</a> / <a target="_blank"  href="http://twitter.com/share?text=&url='.$owner->AbsoluteLink().'">Twitter</a> / <a target="_blank"  href="https://www.linkedin.com/sharing/share-offsite/?url='.$owner->AbsoluteLink().'">LinkedIn</a></p></div>')
+                    ]
+                )->setHeadingLevel(4)
+            );
+
+            // Generate Image field description
+            if($this->owner->findSocialMetaImage() && $this->owner->findSocialMetaImage() != $this->owner->SocialMetaImage()) {
+                $fields->dataFieldByName('SocialMetaImage')->setDescription('Using image: <a target="_blank" href="'.$owner->findSocialMetaImage()->AbsoluteLink().'"><img src="'.$owner->findSocialMetaImage()->ScaleHeight(32)->URL.'">&nbsp;'.$owner->findSocialMetaImage()->Name.'</a>');
+            } else if(!$this->owner->findSocialMetaImage()){
+                $fields->dataFieldByName('SocialMetaImage')->setDescription('If no image is set, Facebook and Twitter will try to detect the main image on the page automatically.');
+            }
+
+            // Generate Description field description
+            if(!$this->owner->findSocialMetaDescription()) {
+                $fields->dataFieldByName('SocialMetaDescription')->setDescription('If no description is set, Facebook and Twitter will try to obtain a description from the page content.');
+            }
         }
+    }
+
+    function findSocialTitleText($forPlaceholder = false) {
+        $owner = $this->getOwner();
+        $className = $owner->ClassName;
+        $configClass = "InnisMaggiore\\SocialMetaTags";
+
+        $titlesConfig = Config::inst()->get($configClass, 'titles') ?: array();
+        $defaultTitle = Config::inst()->get($configClass, 'default_title') ?: "Title";
+
+
+        // 1. Return Social Media Title if specifed in CMS
+        if ($owner->SocialMetaTitle != "" && !$forPlaceholder) {
+            return Convert::raw2att($owner->SocialMetaTitle);
+        
+        // 2. Return class specific title field from YML config
+        } else if (array_key_exists($className, $titlesConfig)) {
+            $titleTextField = $titlesConfig[$className];
+            if (isset($owner->$titleTextField) && $owner->$titleTextField != "") {
+                return Convert::raw2att($owner->$titleTextField);
+            }
+
+        // 3. Return MetaTitle if set in CMS
+        } else if (isset($owner->MetaTitle) && $owner->MetaTitle != "") {
+            return Convert::raw2att($owner->MetaTitle);
+        
+        // 4. Return value from default title field (e.g. Title), if set in YML
+        } else if ($defaultTitle != "" && isset($owner->$defaultTitle) && $owner->$defaultTitle != "") {
+            return Convert::raw2att($owner->$defaultTitle);
+        }
+
+        // 5. Fall back to page Title
+        return $owner->Title;
+    }
+
+    function findSocialMetaDescription($forPlaceholder = false)
+    {
+        $owner = $this->getOwner();
+        $className = $owner->ClassName;
+        $configClass = "InnisMaggiore\\SocialMetaTags";
+        $defaultDescription = Config::inst()->get($configClass, 'default_description') ?: "MetaDescription";
+        $descriptionsConfig = Config::inst()->get($configClass,'descriptions') ?: array();        
+
+        // 1. Return Social Media Description if specifed in CMS
+        if ($owner->SocialMetaDescription != "" && !$forPlaceholder) {
+            return Convert::raw2att($owner->SocialMetaDescription);
+        
+        // 2. Return class specific description field from YML config
+        } else if (array_key_exists($className, $descriptionsConfig)) {
+            $descriptionTextField = $descriptionsConfig[$className];
+            if (isset($owner->$descriptionTextField) && $owner->$descriptionTextField != "") {
+                return Convert::raw2att($owner->$descriptionTextField);
+            }
+        
+        // 3. Return MetaDescription if set in CMS
+        } else if (isset($owner->MetaDescription) && $owner->MetaDescription != "") {
+            return Convert::raw2att($owner->MetaDescription);
+
+        // 4. Return value from default description (e.g. Content), if set in YML
+        } else if ($defaultDescription != "" && isset($owner->$defaultDescription) && $owner->$defaultDescription != "") {
+            if($defaultDescription == 'Content') {
+                return Convert::raw2att(strip_tags($owner->obj('Content')->LimitCharacters(250,'...')));
+            }
+            return Convert::raw2att($owner->$defaultDescription);
+        }
+    }
+
+    function findSocialMetaImage() {
+        $owner = $this->getOwner();
+        $className = $owner->ClassName;
+        $configClass = "InnisMaggiore\\SocialMetaTags";
+        $imagesConfig = Config::inst()->get($configClass, 'images') ?: array();
+
+        
+        // if customized image field isn't populated, fall back to social meta image
+        $image = $owner->SocialMetaImage();
+        if ($image && $image->ID != 0) {
+            return $image;
+        }
+        
+        // get image by customized field
+        if (!isset($imageLink)) {
+            if (array_key_exists($className, $imagesConfig)) {
+                $imageField = $imagesConfig[$className];
+                $image = $owner->$imageField();
+                if ($image && $image->ID != 0) {
+                    return $image;
+                }
+            }
+        }
+
+        return false;
     }
 
     public function MetaTags(&$tags) {
@@ -50,47 +170,15 @@ class SocialMetaTagsExtension extends DataExtension {
             $configClass = "InnisMaggiore\\SocialMetaTags";
             $twitterSite = Config::inst()->get($configClass, 'twitter_site');
 
-            // get defaults
-            $defaultTitle = Config::inst()->get($configClass, 'default_title') ?: "Title";
-            $defaultDescription = Config::inst()->get($configClass, 'default_description') ?: "MetaDescription";
-            $metaDescriptionDefault = Config::inst()->get($configClass, 'meta_description_default');
-
             // get customized fields
-            $descriptionsConfig = Config::inst()->get($configClass,'descriptions') ?: array();
-            $imagesConfig = Config::inst()->get($configClass, 'images') ?: array();
-            $titlesConfig = Config::inst()->get($configClass, 'titles') ?: array();
             $typesConfig = Config::inst()->get($configClass, 'types') ?: array();
 
-            // if customized title field exists and is populated, use it
-            if (array_key_exists($className, $titlesConfig)) {
-                $titleTextField = $titlesConfig[$className];
-                if (isset($owner->$titleTextField) && $owner->$titleTextField != "") {
-                    $titleText = Convert::raw2att($owner->$titleTextField);
-                }
-            }
+            $titleText = $this->findSocialTitleText();
 
-            // fall back to default title field if customized title field doesn't exist or is empty.
-            if (!isset($titleText)) {
-                if ($defaultTitle != "") {
-                    $titleText = Convert::raw2att($owner->$defaultTitle);
-                }
-            }
-
-            // get image by customized field
-            if (array_key_exists($className, $imagesConfig)) {
-                $imageField = $imagesConfig[$className];
-                $image = $owner->$imageField();
-                if ($image && $image->ID != 0) {
-                    $imageLink = $image->AbsoluteLink();
-                }
-            }
-
-            // if customized image field isn't populated, fall back to social meta image
-            if (!isset($imageLink)) {
-                $image = $owner->SocialMetaImage();
-                if ($image && $image->ID != 0) {
-                    $imageLink = $image->AbsoluteLink();
-                }
+            if($this->findSocialMetaImage() && $this->findSocialMetaImage()->exists()) {
+                $imageLink = $this->findSocialMetaImage()->AbsoluteLink();
+            } else {
+                $imageLink = false;
             }
 
             // if customized type is set, use it.
@@ -98,24 +186,8 @@ class SocialMetaTagsExtension extends DataExtension {
                 $ogType = $typesConfig[$className];
             }
 
-            // if customized description field exists and is populated, use it
-            if (array_key_exists($className, $descriptionsConfig)) {
-                $descriptionTextField = $descriptionsConfig[$className];
-                if (isset($owner->$descriptionTextField) && $owner->$descriptionTextField != "") {
-                    $descriptionText = Convert::raw2att($owner->$descriptionTextField);
-                }
-            }
+            $descriptionText = $this->findSocialMetaDescription();
 
-            // fall back to SocialMetaDescription, default description field, and MetaDescription, depending on configs and what is populated
-            if (!isset($descriptionText)) {
-                if ($owner->SocialMetaDescription != "") {
-                    $descriptionText = Convert::raw2att($owner->SocialMetaDescription);
-                } else if ($defaultDescription != "" && isset($owner->$defaultDescription) && $owner->$defaultDescription != "") {
-                    $descriptionText = Convert::raw2att($owner->$defaultDescription);
-                } else if ($metaDescriptionDefault && isset($owner->MetaDescription) && $owner->MetaDescription != "") {
-                    $descriptionText = Convert::raw2att($owner->MetaDescription);
-                }
-            }
 
             // get link
             $link = $owner->AbsoluteLink();
@@ -139,7 +211,7 @@ class SocialMetaTagsExtension extends DataExtension {
             }
 
             // og:image
-            if (isset($imageLink)) {
+            if ($imageLink) {
                 $tags .= "<meta property=\"og:image\" content=\"{$imageLink}\" />\n";
             }
 
