b8b56b151455ddeb6adba55c9c92cff95bf629c5
James Cocker <git@pswd.biz>
2025-10-03 15:07:14 +0100

    Silverstripe 6 Updates
    Rector

--- src/BasicSocialSharingExtension.php
+++ src/BasicSocialSharingExtension.php
@@ -2,11 +2,11 @@
 
 namespace PurpleSpider\BasicSocialSharing;
 
+use SilverStripe\Core\Extension;
 use SilverStripe\Assets\Image;
 use SilverStripe\Core\Convert;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
-use Silverstripe\ORM\DataExtension;
 use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Forms\LiteralField;
@@ -17,19 +17,28 @@ use SilverStripe\CMS\Model\RedirectorPage;
 use SilverStripe\Forms\ToggleCompositeField;
 use SilverStripe\AssetAdmin\Forms\UploadField;
 
-class BasicSocialSharingExtension extends DataExtension {
-    private static $db = array(
+class BasicSocialSharingExtension extends Extension {
+    public $owner;
+
+    private static $db = [
         "SocialMetaTitle" => "Text",
         "SocialMetaDescription" => "Text"
-    );
+    ];
 
-    private static $has_one = array(
+    private static $has_one = [
         "SocialMetaImage"   => Image::class
-    );
+    ];
 
-    private static $owns = array(
+    private static $owns = [
         "SocialMetaImage"
-    );
+    ];
+
+    /**
+     * Disable automatic scaffolding of these fields - we handle them manually
+     */
+    private static array $scaffold_cms_fields_settings = [
+        'ignoreFields' => ['SocialMetaTitle', 'SocialMetaDescription', 'SocialMetaImage'],
+    ];
 
     public function updateCMSFields(FieldList $fields) {
         $owner = $this->getOwner();
@@ -41,8 +50,8 @@ class BasicSocialSharingExtension extends DataExtension {
                     'Social Media Sharing',
                     [
                         LiteralField::create('SocialInfo', '<div class="field" style="margin-bottom:1.23rem"><p class="alert" style="padding-left:0">Customise the details displayed in preview cards when this page is shared on Facebook, Twitter, LinkedIn etc:</p></div>'),
-                        TextField::create("SocialMetaTitle",'Title')->setAttribute('placeholder', $owner->findSocialTitleText($forPlaceholder = true)),
-                        TextareaField::create("SocialMetaDescription",'Description')->setRows(3)->setAttribute('placeholder', $owner->findSocialMetaDescription($forPlaceholder = true)),
+                        TextField::create("SocialMetaTitle",'Title')->setAttribute('placeholder', $owner->findSocialTitleText(true)),
+                        TextareaField::create("SocialMetaDescription",'Description')->setRows(3)->setAttribute('placeholder', $owner->findSocialMetaDescription(true)),
                         UploadField::create("SocialMetaImage",'Image'),
                         LiteralField::create('Debug', '<div class="field" style="margin-bottom:1.23rem"><p class="alert alert-warning">If you change these <i>after</i> sharing the page on Facebook, you may need to instruct Facebook to "re-scrape" this page before it will display the updated details. You can do this using the <strong>Scrape Again</strong> button on the <a href="https://developers.facebook.com/tools/debug/?q=' . $this->owner->AbsoluteLink() . '" target="_blank"><strong>Facebook Sharing Debugger</strong></a>.</div>'),
                         LiteralField::create('ShareLink', '<div class="field" style="margin-bottom:1.23rem"><p style="font-weight:bold; font-size:16px; padding-bottom:3px;display:block;">Share to: <a target="_blank"  href="http://www.facebook.com/share.php?u='.$owner->AbsoluteLink().'">Facebook</a> / <a target="_blank"  href="http://twitter.com/share?text=&url='.$owner->AbsoluteLink().'">Twitter</a> / <a target="_blank"  href="https://www.linkedin.com/sharing/share-offsite/?url='.$owner->AbsoluteLink().'">LinkedIn</a></p></div>')
@@ -51,45 +60,43 @@ class BasicSocialSharingExtension extends DataExtension {
             );
 
             // Generate Image field description
-            if($this->owner->findSocialMetaImage() && $this->owner->findSocialMetaImage() != $this->owner->SocialMetaImage()) {
+            if ($this->owner->findSocialMetaImage() && $this->owner->findSocialMetaImage() != $this->owner->SocialMetaImage()) {
                 $fields->dataFieldByName('SocialMetaImage')->setDescription('Using image: <a target="_blank" href="'.$owner->findSocialMetaImage()->AbsoluteLink().'"><img src="'.$owner->findSocialMetaImage()->ScaleHeight(32)->URL.'">&nbsp;'.$owner->findSocialMetaImage()->Name.'</a>');
-            } else if(!$this->owner->findSocialMetaImage()){
+            } elseif (!$this->owner->findSocialMetaImage()) {
                 $fields->dataFieldByName('SocialMetaImage')->setDescription('If no image is set, Facebook and Twitter will try to detect the main image on the page automatically.');
             }
 
             // Generate Description field description
-            if(!$this->owner->findSocialMetaDescription()) {
+            if (!$this->owner->findSocialMetaDescription()) {
                 $fields->dataFieldByName('SocialMetaDescription')->setDescription('If no description is set, Facebook and Twitter will try to obtain a description from the page content.');
             }
         }
     }
 
-    function findSocialTitleText($forPlaceholder = false) {
+    public function findSocialTitleText($forPlaceholder = false) {
         $owner = $this->getOwner();
         $className = $owner->ClassName;
         $configClass = "PurpleSpider\\BasicSocialSharing";
 
-        $titlesConfig = Config::inst()->get($configClass, 'titles') ?: array();
+        $titlesConfig = Config::inst()->get($configClass, 'titles') ?: [];
         $defaultTitle = Config::inst()->get($configClass, 'default_title') ?: "Title";
 
 
         // 1. Return Social Media Title if specifed in CMS
         if ($owner->SocialMetaTitle != "" && !$forPlaceholder) {
             return Convert::raw2att($owner->SocialMetaTitle);
-        
-        // 2. Return class specific title field from YML config
-        } else if (array_key_exists($className, $titlesConfig)) {
+            // 2. Return class specific title field from YML config
+        } elseif (array_key_exists($className, $titlesConfig)) {
             $titleTextField = $titlesConfig[$className];
             if (isset($owner->$titleTextField) && $owner->$titleTextField != "") {
                 return Convert::raw2att($owner->$titleTextField);
             }
 
-        // 3. Return MetaTitle if set in CMS
-        } else if (isset($owner->MetaTitle) && $owner->MetaTitle != "") {
+            // 3. Return MetaTitle if set in CMS
+        } elseif (isset($owner->MetaTitle) && $owner->MetaTitle != "") {
             return Convert::raw2att($owner->MetaTitle);
-        
-        // 4. Return value from default title field (e.g. Title), if set in YML
-        } else if ($defaultTitle != "" && isset($owner->$defaultTitle) && $owner->$defaultTitle != "") {
+            // 4. Return value from default title field (e.g. Title), if set in YML
+        } elseif ($defaultTitle != "" && isset($owner->$defaultTitle) && $owner->$defaultTitle != "") {
             return Convert::raw2att($owner->$defaultTitle);
         }
 
@@ -97,45 +104,45 @@ class BasicSocialSharingExtension extends DataExtension {
         return $owner->Title;
     }
 
-    function findSocialMetaDescription($forPlaceholder = false)
+    public function findSocialMetaDescription($forPlaceholder = false)
     {
         $owner = $this->getOwner();
         $className = $owner->ClassName;
         $configClass = "PurpleSpider\\BasicSocialSharing";
         $defaultDescription = Config::inst()->get($configClass, 'default_description') ?: "MetaDescription";
-        $descriptionsConfig = Config::inst()->get($configClass,'descriptions') ?: array();        
+        $descriptionsConfig = Config::inst()->get($configClass,'descriptions') ?: [];        
 
         // 1. Return Social Media Description if specifed in CMS
         if ($owner->SocialMetaDescription != "" && !$forPlaceholder) {
             return Convert::raw2att($owner->SocialMetaDescription);
-        
-        // 2. Return class specific description field from YML config
-        } else if (array_key_exists($className, $descriptionsConfig)) {
+            // 2. Return class specific description field from YML config
+        } elseif (array_key_exists($className, $descriptionsConfig)) {
             $descriptionTextField = $descriptionsConfig[$className];
             if (isset($owner->$descriptionTextField) && $owner->$descriptionTextField != "") {
                 return Convert::raw2att($owner->$descriptionTextField);
             }
-        
-        // 3. Return MetaDescription if set in CMS
-        } else if (isset($owner->MetaDescription) && $owner->MetaDescription != "") {
-            return Convert::raw2att($owner->MetaDescription);
 
-        // 4. Return value from default description (e.g. Content), if set in YML
-        } else if ($defaultDescription != "" && isset($owner->$defaultDescription) && $owner->$defaultDescription != "") {
+            // 3. Return MetaDescription if set in CMS
+        } elseif (isset($owner->MetaDescription) && $owner->MetaDescription != "") {
+            return Convert::raw2att($owner->MetaDescription);
+            // 4. Return value from default description (e.g. Content), if set in YML
+        } elseif ($defaultDescription != "" && isset($owner->$defaultDescription) && $owner->$defaultDescription != "") {
             if($defaultDescription == 'Content') {
-                $description = strip_tags($owner->obj('Content')->LimitCharacters(250, '...'));
-                $description = str_replace(array("\r", "\n"), '', $description);
-                return $description;
+                $description = strip_tags((string) $owner->obj('Content')->LimitCharacters(250, '...'));
+                return str_replace(["\r", "\n"], '', $description);
             }
+
             return Convert::raw2att($owner->$defaultDescription);
         }
+
+        return null;
     }
 
-    function findSocialMetaImage() {
+    public function findSocialMetaImage() {
         $owner = $this->getOwner();
         $className = $owner->ClassName;
         $configClass = "PurpleSpider\\BasicSocialSharing";
-        $imagesConfig = Config::inst()->get($configClass, 'images') ?: array();
+        $imagesConfig = Config::inst()->get($configClass, 'images') ?: [];
 
         
         // if customized image field isn't populated, fall back to social meta image
@@ -145,13 +152,11 @@ class BasicSocialSharingExtension extends DataExtension {
         }
         
         // get image by customized field
-        if (!isset($imageLink)) {
-            if (array_key_exists($className, $imagesConfig)) {
-                $imageField = $imagesConfig[$className];
-                $image = $owner->$imageField();
-                if ($image && $image->ID != 0) {
-                    return $image;
-                }
+        if (!isset($imageLink) && array_key_exists($className, $imagesConfig)) {
+            $imageField = $imagesConfig[$className];
+            $image = $owner->$imageField();
+            if ($image && $image->ID != 0) {
+                return $image;
             }
         }
 
@@ -173,7 +178,7 @@ class BasicSocialSharingExtension extends DataExtension {
             $twitterSite = Config::inst()->get($configClass, 'twitter_site');
 
             // get customized fields
-            $typesConfig = Config::inst()->get($configClass, 'types') ?: array();
+            $typesConfig = Config::inst()->get($configClass, 'types') ?: [];
 
             $titleText = $this->findSocialTitleText();
 
